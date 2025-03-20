98abe4c13e26eeb78408822466e5fa77d932feb7
Karlo Mrakovčić <mrakovcickarlo@gmail.com>
2025-03-20 16:01:07 +0100

    Initial commit

--- Karlo/scripts/slurm_opacity_grid.sh
+++ Karlo/scripts/slurm_opacity_grid.sh
@@ -6,7 +6,6 @@
 #SBATCH --ntasks=1
 #SBATCH --time=336:00:00
 srun hostname
-srun date
-srun python3 /home/kmrakovcic/Projects/ANN-VIL/scripts/opacity_grid_generation.py \
+python3 /home/kmrakovcic/Projects/ANN-VIL/scripts/opacity_grid_generation.py \
 --E_num 1000 \
 --Eqg_num 1000
